231730d05b1c6e029ddf6beda9928bb9e53f1545
Artjohn Clark Dinorog <144906804+Deucariousity@users.noreply.github.com>
2023-09-27 21:44:08 +0800

    Delete DINOROG_MinSecConverter.py

--- DINOROG_MinSecConverter.py
+++
@@ -1,5 +0,0 @@
-print("Minutes and Seconds Converter")
-integer_seconds = int(input("Enter an integer for seconds:"))
-minutes = integer_seconds // 60
-seconds = minutes % 6 + 18
-print(integer_seconds, "seconds is", int(minutes), "minutes and", int(seconds), "seconds.")
